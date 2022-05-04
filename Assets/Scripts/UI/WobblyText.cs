64a9bb3924d24e5df70c5c76fc982ae938a313f6
VivatikPumpkinEater <vivatik.pumpkin.eater@gmail.com>
2022-05-04 10:38:25 +0300

    Add TMP effects (beta 0.1)

--- Assets/Scripts/UI/WobblyText.cs
+++ Assets/Scripts/UI/WobblyText.cs
@@ -3,32 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Random = UnityEngine.Random;
 
 public class WobblyText : MonoBehaviour
 {
-    [SerializeField] private ETextEffect _textEffect = ETextEffect.Wobbly;
-    
-    [SerializeField] private TMP_Text _textComponent = null;
-
-    [Header("WobblySettings")] [Space(15f)] [SerializeField]
-    private Vector2 _waveSpeed = new Vector2(0.01f, 2f);
-
-    [SerializeField] private float _multiply = 10f;
+    [SerializeField] private List<TextEffector> _textComponents = new List<TextEffector>();
 
     private void Update()
     {
-        switch (_textEffect)
+        foreach (var text in _textComponents)
         {
-            case ETextEffect.Wobbly:
-                Wobbly();
-                break;
+            switch (text.TextEffect)
+            {
+                case ETextEffect.Wobbly:
+                    Wobbly(text.TextComponent, text.WobblySetting, text.Multiply);
+                    break;
+                case ETextEffect.WobblyV2:
+                    WobblyV2(text.TextComponent);
+                    break;
+                case ETextEffect.Shaking:
+                    Shaking(text.TextComponent, text.WobblySetting, text.Multiply);
+                    break;
+                case ETextEffect.VertexWobbly:
+                    VertexWobbly(text.TextComponent, text.WobblySetting);
+                    break;
+            }
         }
     }
 
-    private void Wobbly()
+    private void Wobbly(TMP_Text text, Vector2 waveSpeed, float multiply)
     {
-        _textComponent.ForceMeshUpdate();
-        var textInfo = _textComponent.textInfo;
+        text.ForceMeshUpdate();
+        var textInfo = text.textInfo;
 
         for (int i = 0; i < textInfo.characterCount; ++i)
         {
@@ -45,22 +51,125 @@ public class WobblyText : MonoBehaviour
             {
                 var origin = verts[characterInfo.vertexIndex + j];
                 verts[characterInfo.vertexIndex + j] =
-                    origin + new Vector3(0, Mathf.Sin(Time.time * _waveSpeed.y + origin.x * _waveSpeed.x) * _multiply, 0);
+                    origin + new Vector3(0, Mathf.Sin(Time.time * waveSpeed.y + origin.x * waveSpeed.x) * multiply, 0);
             }
-            
         }
 
         for (int i = 0; i < textInfo.meshInfo.Length; ++i)
         {
             var meshInfo = textInfo.meshInfo[i];
             meshInfo.mesh.vertices = meshInfo.vertices;
-            
-            _textComponent.UpdateGeometry(meshInfo.mesh, i);
+
+            text.UpdateGeometry(meshInfo.mesh, i);
         }
     }
+
+    private void Shaking(TMP_Text text, Vector2 waveSpeed, float multiply)
+    {
+        text.ForceMeshUpdate();
+        var textInfo = text.textInfo;
+
+        for (int i = 0; i < textInfo.characterCount; ++i)
+        {
+            var characterInfo = textInfo.characterInfo[i];
+
+            if (!characterInfo.isVisible)
+            {
+                continue;
+            }
+
+            var verts = textInfo.meshInfo[characterInfo.materialReferenceIndex].vertices;
+
+            for (int j = 0; j < 4; ++j)
+            {
+                int offset = 0;
+                while (offset == 0)
+                {
+                    offset = Random.Range(-1, 2);
+                }
+                
+                var origin = verts[characterInfo.vertexIndex + j];
+                verts[characterInfo.vertexIndex + j] =
+                    origin + new Vector3(Mathf.Sin(Time.time * waveSpeed.y * offset + origin.y * waveSpeed.x) * multiply,
+                        Mathf.Sin(Time.time * waveSpeed.y * -offset + origin.y * waveSpeed.x) * multiply, 0);
+            }
+        }
+
+        for (int i = 0; i < textInfo.meshInfo.Length; ++i)
+        {
+            var meshInfo = textInfo.meshInfo[i];
+            meshInfo.mesh.vertices = meshInfo.vertices;
+
+            text.UpdateGeometry(meshInfo.mesh, i);
+        }
+    }
+    
+    private void WobblyV2(TMP_Text text)
+    {
+        Mesh mesh;
+
+        Vector3[] vertices;
+        
+        text.ForceMeshUpdate();
+        mesh = text.mesh;
+        vertices = mesh.vertices;
+
+        for (int i = 0; i < text.textInfo.characterCount; i++)
+        {
+            TMP_CharacterInfo c = text.textInfo.characterInfo[i];
+
+            int index = c.vertexIndex;
+
+            Vector3 offset = Wobble(Time.time + i);
+            vertices[index] += offset;
+            vertices[index + 1] += offset;
+            vertices[index + 2] += offset;
+            vertices[index + 3] += offset;
+        }
+
+        mesh.vertices = vertices;
+        text.canvasRenderer.SetMesh(mesh);
+    }
+
+    private void VertexWobbly(TMP_Text text, Vector2 waveSpeed)
+    {
+        Mesh mesh;
+
+        Vector3[] vertices;
+        
+        text.ForceMeshUpdate();
+        mesh = text.mesh;
+        vertices = mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 offset = Wobble(Time.time + i * waveSpeed.y);
+
+            vertices[i] = vertices[i] + offset;
+        }
+
+        mesh.vertices = vertices;
+        text.canvasRenderer.SetMesh(mesh);
+    }
+
+    Vector2 Wobble(float time) {
+        return new Vector2(Mathf.Sin(time*3.3f), Mathf.Cos(time*2.5f));
+    }
+}
+
+[Serializable]
+public struct TextEffector
+{
+    public TMP_Text TextComponent;
+    public ETextEffect TextEffect;
+    public Vector2 WobblySetting;
+    public float Multiply;
 }
 
 public enum ETextEffect
 {
-    Wobbly
+    Wobbly,
+    WobblyV2,
+    VertexWobbly,
+    Shaking
 }
